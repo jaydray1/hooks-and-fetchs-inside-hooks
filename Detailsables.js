34b4fa62d1b7dcaacfccf83dc29b81231ad010b0
jaydray1 <jdreher15@gmail.com>
2019-11-20 07:58:45 -0800

    add details component

--- Detailsables.js
+++ Detailsables.js
@@ -3,22 +3,20 @@ import styled from "styled-components";
 
 const StyledDetails = styled.div`
 display: flex;
+flex-direction: column;
 height: 100%;
-width: 75%;
-border: 2px solid red;
+width: 40em;
+border: 2px solid orange;
 `;
 
 const Detailsables = ({ oneAlien }) => {
 
-  React.useEffect(() => {
-    console.log('this is the one alien', oneAlien);
-  }, [oneAlien])
   return (
-    <StyledDetails>
-      <h3>{oneAlien.birth_year}</h3>
-      <h3>{oneAlien.eye_color}</h3>
-      <h3>{oneAlien.eye_color}</h3>
-    </StyledDetails>
+      <StyledDetails>
+        <h3>Date of Birth:{oneAlien.birth_year}</h3>
+        <h3>Height:{oneAlien.height}</h3>
+        <h3>Eye Color:{oneAlien.eye_color}</h3>
+      </StyledDetails>
   )
 }
 
